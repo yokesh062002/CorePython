0a0959e3e297b7f3bbfca0be71e56dcb1ae1d36d
yokesh062002 <yokesh4511@gmail.com>
2023-09-06 10:40:26 -0700

    Biodataandbasic

---
+++ Basic/bio data.py
@@ -0,0 +1,4 @@
+print("yokesh")
+print("s/o Panner selvam")
+print("5/23B, selvam maligai kadai near,Jagir kamanayakken Patti,Salem")
+print("ph.no:8870570623")
---
+++ Basic/hello.py
@@ -0,0 +1,7 @@
+print("Welcome to zealous")
+print("Yogesh")
+a=10
+print(type(a))
+
+b="Annamalai"
+print(type(b))
