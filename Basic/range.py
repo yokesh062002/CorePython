378407ea9add0e71f604dcf829b9f76181e17a45
yokesh062002 <yokesh4511@gmail.com>
2023-09-21 10:57:16 -0700

    dd

---
+++ Basic/range.py
@@ -0,0 +1,7 @@
+for i in range(1,10):
+    print(i)
+
+a=["yokesh","tamil","ravi"]
+for i in range(len(a)):
+    print("welcome",a[i])
+    print(len(a))
---
+++ Basic/table.py
@@ -0,0 +1,4 @@
+a=int(input("enter the table no:"))
+for i in range(1,11):
+    temp=a*i
+    print(a,"*",i,"=",temp)
